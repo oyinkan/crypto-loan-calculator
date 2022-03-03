bed82d2d06adf470b41cea01c49e7c0420ebbc17
oyinkan <skusman@student.lautech.edu.ng>
2022-03-03 09:32:30 +0100

    made changes to the hero text

--- src/components/landing-page/Index.js
+++ src/components/landing-page/Index.js
@@ -7,7 +7,7 @@ export default function Index() {
       <div className="wrapper block md:flex items-center p-12 ">
         <div className="w-full md:w-1/2">
           <h1 className="text-white text-5xl mb-6">Crypto Loan<br/> Calculator Solution</h1>
-          <p className="text-white text-xl pr-12">This is a research, development and solutions provider company with a vibrant research and development pipeline</p>
+          <p className="text-white text-xl pr-12">Easily find out the interest rate(%) for your crypto loans</p>
         </div>
         <div className="w-full md:w-1/2">
           <CalculatorForm />
